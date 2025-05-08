2bc31b0ecb25d5ac244ea1fc6fc2830be0545fdf
Jeppe Brage Christensen <jbrage@users.noreply.github.com>
2025-05-08 11:59:11 +0200

    add point

--- mypackage/geometry/geometry.py
+++ mypackage/geometry/geometry.py
@@ -8,7 +8,7 @@ class Point:
 
 
 class Line:
-    def __init__(self, p1: "Point", p2: "Point") -> None:
+    def __init__(self, p1: Point, p2: Point) -> None:
         self.p1 = p1
         self.p2 = p2
 
