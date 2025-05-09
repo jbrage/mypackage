8c49e79b4d36f5b780248fa4b5c215debab5348b
Jeppe Brage Christensen <jbrage@users.noreply.github.com>
2025-05-09 15:21:04 +0200

    testing

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,24 +1,17 @@
-# Ensure pytest-xdist is installed to enable parallel test execution
-# Run tests with: pytest -n <num_processes>
-from mypackage.geometry import Point
-from mypackage.utils import distance
-from time import sleep
-
-
 def test_something():
-    sleep(5)
+    # sleep(5)
     assert True
 
 
-def test_distance():
-    p1 = Point(1, 2)
-    p2 = Point(4, 6)
-    sleep(5)
-    assert distance(p1, p2) == 5.0
+# def test_distance():
+#     p1 = Point(1, 2)
+#     p2 = Point(4, 6)
+#     sleep(5)
+#     assert distance(p1, p2) == 5.0
 
 
-def new_test():
-    p1 = Point(0, 0)
-    p2 = Point(3, 4)
-    sleep(5)
-    assert distance(p1, p2) == 5.0
+# def new_test():
+#     p1 = Point(0, 0)
+#     p2 = Point(3, 4)
+#     sleep(5)
+#     assert distance(p1, p2) == 5.0
